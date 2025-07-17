4407a95472f66c875400290391592c7bfbc4425f
SylvesterFraide <fridaysylvester2@gmail.com>
2025-07-17 17:31:58 +0300

    changes

--- src/component/create.js
+++ src/component/create.js
@@ -1,39 +1,66 @@
-import React, { useState } from 'react'
-import { Data } from './data';
-import { Link, useNavigate } from 'react-router-dom';
-import { v4 as uuid } from 'uuid';
+import React, { useState } from "react";
+import { Data } from "./data";
+import { Link, useNavigate } from "react-router-dom";
+import { v4 as uuid } from "uuid";
 
 const Create = () => {
+  //making usestate for setting and fetching a value in jsx
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
 
-    //making usestate for setting and fetching a value in jsx
-    const [name, setName] = useState('');
-    const [age, setAge] = useState('');
+  //using usenavigate for redirecting to pages
+  let history = useNavigate();
 
-    //using usenavigate for redirecting to pages
-    let history = useNavigate();
+  //function for creating a post/entry
+  const handleSubmit = (e) => {
+    e.preventDefault(); // prevent reload
 
-    //function for creating a post/entry
-    const handleSubmit = (e) => {
-        e.preventDefault(); // prevent reload
+    const ids = uuid(); // creating unique id
+    let uni = ids.slice(0, 8); //slicing unique id
 
-        const ids = uuid(); // creating unique id
-        let  uni = ids.slice(0, 8) //slicing unique id
+    //fetching a value from usestate and pushing to javascript object
+    let a = name,
+      b = age;
+    if (name === "" || age === "") {
+      alert("invalid input");
+      return;
+    }
+    Data.push({ id: uni, Name: a, Age: b });
 
-        //fetching a value from usestate and pushing to javascript object
-        let a = name, b = age;
-        if (name == '' || age == '') {
-            alert('invalid input');
-            return;
-        }
-            Data.push({id:uni, Name: a, Age: b});
-
-            //redirecting to home page after creating done
-            history('/');
-    };
+    //redirecting to home page after creating done
+    history("/");
+  };
 
   return (
-    <div>Create</div>
-  )
-}
+    <div>
+      <form>
+        <input
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Enter Nmane"
+          required
+        />
 
+        <input
+          onChange={(e) => setAge(e.target.value)}
+          type="number"
+          placeholder="Age"
+          required
+        />
+
+        <button type="submit" onClick={(e) => handleSubmit(e)}>
+          Submit
+        </button>
+
+        {/*redirecting back to home page*/}
+        <Link to="/home">
+          <button>
+            Home
+          </button>
+        </Link>
+
+      </form>
+    </div>
+  );
+};
 export default Create;
--- src/component/home.js
+++ src/component/home.js
@@ -1,8 +1,76 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { Data } from './data';
 
 const Home = () => {
+  let history = useNavigate();
+
+  //function to set the id, name, and age in local storage
+  function setID(id, name, age) {
+    localStorage.setItem("id", id);
+    localStorage.setItem("Name", name);
+    localStorage.setItem("Age", age);
+  }
+
+  //function to delete to delete an entry
+  function deleted(id) {
+    let index = Data
+    .map(function (e) {
+      return e.id;
+    }) 
+    .indexOf(id); 
+
+    //deleting the entry with the specific index
+    Data.splice(index, 1);
+
+    //redirecting to the same page to re-render
+    history("/");
+  }
+
   return (
-    <div>Home</div>
+    <div className='home'>
+      <h1>User Management</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            Data.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{item.Name}</td>
+                  <td>{item.Age}</td>
+                  <td>
+                    <Link  to= {'/edit'}>
+                      <button onClick={() => setID(item.id, item.Name, item.Age )}>
+          
+                        Update
+                      </button>
+                    </Link>
+
+                    <button onClick={() => deleted(item.id)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+      <div className='create-user'>
+        <Link to={"/create"}>
+          <button>
+            Create New User
+          </button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
